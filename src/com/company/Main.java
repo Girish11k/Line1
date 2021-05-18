f1efc348fe760c8a3f18cea49c7d003ee3ddd631
Girish11k <68158466+Girish11k@users.noreply.github.com>
2021-05-18 18:14:05 +0530

    Commit Line1 push

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int x1,x2,y1,y2;
+        double dis;
+        x1=0;y1=0;x2=2;y2=1;
+        dis=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+         System.out.println("distancebetween"+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+dis);
+}
+}
+
+
+
